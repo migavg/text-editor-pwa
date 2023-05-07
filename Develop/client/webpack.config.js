7704a96bd296bac73508069a6bbe71b03468495e
mvill <119754846+migavg@users.noreply.github.com>
2023-05-06 23:02:52 -0700

    added module css loaders

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-   
+
     //html webpack
     plugins: [
       new HtmlWebpackPlugin({
@@ -52,7 +52,23 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+
       ],
     },
   };
